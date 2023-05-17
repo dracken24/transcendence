15842bdb1abc41ba177bb9294db0696d773a37ac
nadia <dracken24@gmail.com>
2023-05-17 11:22:09 -0400

    add js for pong

--- srcs/test.js
+++
@@ -1,9 +0,0 @@
-let http = require('http');
-
-http.createServer(function (req, res)
-{
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('GO GO POWER RANGER!!!');
-})
-
-.listen(8080);
